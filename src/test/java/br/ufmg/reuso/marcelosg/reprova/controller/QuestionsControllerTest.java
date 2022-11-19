f62fb9dc931e0f44d4af20d5e9b7591eaadb97cf
Felipe Sena <felipedepaulasena@hotmail.com>
2022-11-19 18:48:29 -0300

    Fix tests

--- src/test/java/br/ufmg/reuso/marcelosg/reprova/controller/QuestionsControllerTest.java
+++ src/test/java/br/ufmg/reuso/marcelosg/reprova/controller/QuestionsControllerTest.java
@@ -1,5 +1,7 @@
 package br.ufmg.reuso.marcelosg.reprova.controller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,20 +10,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import org.json.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureDataMongo
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
 class QuestionsControllerTest {
     @Autowired
     private MongoTemplate mongoTemplate;
