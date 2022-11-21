34fb91c2a2ce29d6b8a2d71916b606fe0cea9691
Marcelo Lima <marcelo.lima@hotmart.com>
2022-11-21 12:04:08 -0300

    added unit tests for CalculateExamGrades, GenerateExamTest and AddGradesTest

---
+++ src/test/java/br/ufmg/reuso/marcelosg/reprova/unit/ExamServiceTests/GenerateExamTest.java
@@ -0,0 +1,49 @@
+package br.ufmg.reuso.marcelosg.reprova.unit.ExamServiceTests;
+
+import br.ufmg.reuso.marcelosg.reprova.model.ExamGeneratorCriteria;
+import br.ufmg.reuso.marcelosg.reprova.service.ExamService;
+import br.ufmg.reuso.marcelosg.reprova.strategies.ExamGeneratorStrategyRegistry;
+import br.ufmg.reuso.marcelosg.reprova.strategies.RandomExamGeneratorStrategy;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class GenerateExamTest {
+
+    @InjectMocks
+    ExamService examService;
+
+    @Mock
+    ExamGeneratorStrategyRegistry strategyRegistry;
+
+    @Mock
+    RandomExamGeneratorStrategy randomExamGeneratorStrategy;
+
+    @Test
+    public void generateExam_whenCriteriaIsValid_shouldBeSuccessful() {
+        // arrange
+        var criteria = new ExamGeneratorCriteria(10, "Test",
+                2022, 2, "This is a test", false, null);
+
+        when(randomExamGeneratorStrategy.generateExamQuestions(any())).thenReturn(List.of());
+        when(strategyRegistry.getStrategy(anyString())).thenReturn(randomExamGeneratorStrategy);
+
+        // act
+        var exam = examService.generateExam(criteria);
+
+        // assert
+        assertEquals("This is a test", exam.getTitle());
+        assertTrue(exam.getQuestions().isEmpty());
+    }
+}
---
+++ src/test/java/br/ufmg/reuso/marcelosg/reprova/unit/QuestionServiceTests/AddGradesTest.java
@@ -0,0 +1,47 @@
+package br.ufmg.reuso.marcelosg.reprova.unit.QuestionServiceTests;
+
+import br.ufmg.reuso.marcelosg.reprova.exceptions.ItemNotFoundException;
+import br.ufmg.reuso.marcelosg.reprova.model.SemesterGrade;
+import br.ufmg.reuso.marcelosg.reprova.model.Stats;
+import br.ufmg.reuso.marcelosg.reprova.repository.QuestionRepository;
+import br.ufmg.reuso.marcelosg.reprova.service.QuestionService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AddGradesTest {
+
+    @Mock
+    QuestionRepository questionRepository;
+
+    @InjectMocks
+    QuestionService questionService;
+
+    @Test
+    public void addGrades_whenQuestionIsNotFound_shouldThrowException() {
+        // arrange
+        when(questionRepository.findById(anyString())).thenReturn(Optional.ofNullable(null));
+
+        assertThrows(ItemNotFoundException.class, () -> questionService.addGrades("1", getSampleGrade()));
+    }
+
+    private SemesterGrade getSampleGrade() {
+        return SemesterGrade.builder()
+                .discipline("Test")
+                .year(2022)
+                .grades(List.of())
+                .semester(1)
+                .stats(Stats.builder().max(1D).mean(1D).median(1D).min(1D).standardDeviation(0D).build())
+                .build();
+    }
+}
